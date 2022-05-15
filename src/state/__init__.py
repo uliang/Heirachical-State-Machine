86dca4207f9e19c60e583543d2ca1da79878af51
uliang <uliang6482@gmail.com>
2022-05-15 11:39:27 +0800

    Patches State, StateMachine and StateMachineBuilder class
    Reimplemtation of state machine builder logic because have refactored
    state tree logic to seperate Tree class. This is in preparation for
    using the Tree class to encapsulate LCA algorithms when doing transition
    logic.

--- src/state/__init__.py
+++ src/state/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import dataclasses
+from typing import Optional
 
 from blinker.base import Signal
 
@@ -14,71 +15,41 @@ UNSET_EVENT = Event(UNSET)
 @dataclasses.dataclass(eq=True)
 class State: 
     name: str = '' 
-    _children: list[State] = dataclasses.field(
-            default_factory=list, 
-            compare=False, 
-            repr=True)
-
-    parent: State = dataclasses.field(
-            compare=False, 
-            repr=False, 
-            init=False)
-
-    def add_substate(self, substate:State)->State: 
-        self._children.append(substate)
-        substate.parent = self
-        return substate
+    depth: int = dataclasses.field(default=0, 
+        compare=False, 
+        init=False, 
+        repr=True)
 
     def should_initially_enter(self) -> bool: 
         return False
-   
-    @property
-    def is_leaf(self) -> bool: 
-        return not bool(self._children)
-
-    def __iter__(self): 
-        return iter(self._children)
 
 
-UNSET_STATE = State(UNSET)
+
+UNSET = State('UNSET')
 UNHANDLED_STATE = State(UNHANDLED)
-
 
 @dataclasses.dataclass
 class InitialState: 
     component: State = dataclasses.field(default=None, 
             init=True, repr=False) 
-
-    def add_substate(self, substate:State): 
-        self.component.add_substate(substate)
     
     def should_initially_enter(self) -> bool: 
         return True  
 
     @property 
-    def is_leaf(self) -> bool: 
-        return self.component.is_leaf 
+    def depth(self) -> int: 
+        return self.component.depth 
 
-    def __iter__(self): 
-        return iter(self.component)
-
-    def __eq__(self, state:State) -> bool: 
-        return self.component == state
+    @depth.setter
+    def depth(self, value:int): 
+        self.component.depth = value
 
     def __repr__(self) -> str: 
         return repr(self.component)
 
-    @property
-    def _children(self)-> list[State]: 
-        return self.component._children
+    def __eq__(self, other:State) -> bool: 
+        return self.component == other 
 
-    @property
-    def parent(self) -> State: 
-        return self.component.parent
-
-    @parent.setter
-    def parent(self, state: State): 
-        self.component.parent = state
 
 @dataclasses.dataclass
 class Transition: 
@@ -93,43 +64,64 @@ class Transition:
                 return UNHANDLED_STATE 
         return self.dest
 
+@dataclasses.dataclass
+class Tree: 
+    _vertices: list[State] = dataclasses.field(
+        default_factory=list) 
+    _edges:list[tuple[State, State]] = dataclasses.field(
+        default_factory=list) 
+    _ROOT: State = dataclasses.field(
+        default=State('ROOT'))
+
+    def children(self, this_node:State) -> list[State]: 
+        return [child for parent, child in self._edges 
+            if parent == this_node 
+            and child.depth > this_node.depth]
+
+    def parent(self, this_node: State) -> State: 
+        return next(parent for parent,child in self._edges
+            if child == this_node 
+            and parent.depth < this_node.depth)
+
+    def leaf(self, this_node: State) -> bool: 
+        return not bool(self.children(this_node))
+
 
 @dataclasses.dataclass
 class StateMachine: 
-    parent = None 
-    _children: list[State] = dataclasses.field(
-            default_factory=list, 
-            init=False)
-    current_state: State = dataclasses.field(
-        default=UNSET_STATE, 
+    _state_tree: Tree = dataclasses.field(
+        default_factory=Tree, 
+        init=False)
+
+    _current_state: State = dataclasses.field(
+        default=UNSET, 
         init=False) 
-    _transition_registry: dict[str, Transition] = dataclasses.field(default_factory=dict) 
+    _transition_registry: dict[str, Transition] = \
+        dataclasses.field(default_factory=dict, 
+        init=False) 
 
     def start(self): 
-        head = self 
-        level = self._children 
-        while not head.is_leaf: 
-            for child_state in level:
-                if child_state.should_initially_enter():
-                    head = child_state
-                    level = head._children
+        children = self._state_tree.children
+        leaf = self._state_tree.leaf
+        head = self._state_tree._ROOT
+
+        while not leaf(head): 
+            for child in children(head):
+                if child.should_initially_enter():
+                    head = child
                     break
-        self.current_state = head  
+        self._current_state = head  
     
     def dispatch(self, sender, event:Event): 
         dest_state = self._transition_registry[event.name].do_transition(self)
         if dest_state is not UNHANDLED_STATE: 
-            self.current_state = dest_state
+            self._current_state = dest_state
 
     def subscribe(self, event_emitter:Signal): 
         event_emitter.connect(self.dispatch)
 
     def get_current_state(self) -> State: 
-        return self.current_state
-
-    @property
-    def is_leaf(self) -> bool: 
-        return not bool(self._children)
+            return self._current_state
 
 
 @dataclasses.dataclass
@@ -138,8 +130,12 @@ class StateMachineBuilder:
     machine: StateMachine = dataclasses.field(
             default_factory=StateMachine)
 
-    def add_state(self, state:State): 
-        self.machine._children.append(state)
+    def add_state(self, state:State, *, substate_of: Optional[State] = None) -> State: 
+        parent = substate_of if substate_of else self.machine._state_tree._ROOT
+        state.depth = parent.depth + 1
+        self.machine._state_tree._vertices.append(state) 
+        self.machine._state_tree._edges.append((parent, state))
+        return state
 
     def get_machine(self) -> StateMachine: 
         return self.machine 
