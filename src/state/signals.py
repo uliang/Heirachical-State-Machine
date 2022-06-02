20dcbc3084386993364d5b9656b64a356fc3db5a
Tang U-Liang <uliang6482@gmail.com>
2022-06-02 20:28:11 +0800

    Uses INTERPRET_STATE_MACHINE_CONFIG
    We use the INTERPRET_STATE_MACHINE_CONFIG to enable the interpreter
    callable to be called at Entity's lifecycle.

--- src/state/signals.py
+++ src/state/signals.py
@@ -3,4 +3,6 @@ import blinker
 
 
 ADD_STATE = blinker.Signal('Emitted when state is to be added to the repository tree')   
-GET_STATE = blinker.Signal('Emiited when a certain state is requested')
+GET_STATE = blinker.Signal('Emiited when state is queried with state_id')
+INTERPRET_STATE_MACHINE_CONFIG = blinker.Signal(
+        'Emitted when metaclass needs to initialize the state machine.')
