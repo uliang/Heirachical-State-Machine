155fef6123e4f8d486c81be19575a47fc8e2890b
Tang U-Liang <uliang6482@gmail.com>
2022-06-09 20:48:56 +0800

    Refactor repository
    Repository no longer has access to its tree's private variables.

--- src/state/repository.py
+++ src/state/repository.py
@@ -16,20 +16,17 @@ class StateRepository:
     @classmethod 
     def with_connections(cls): 
         repo = cls()
-        ADD_STATE.connect(repo.add_state)
-        GET_STATE.connect(repo.get_state)        
+        ADD_STATE.connect(repo.insert)
+        GET_STATE.connect(repo.get)        
         FLUSH_DATABASE.connect(repo.flush)
         return repo
 
-    def add_state(self, sender:State, /,  entity_name:str, name:str): 
+    def insert(self, sender:State, /,  entity_name:str, name:str): 
         tree = self._database[entity_name] 
-        tree._vertices[name] = sender 
-        parent_name = sender.substate_of
-        # parent = tree._vertices[parent_name] 
-        tree._edges.append((parent_name, name))
+        tree.add_vertex(sender, name, parent_name=sender.substate_of)
 
-    def get_state(self, sender, /, entity_name:str, name:str): 
-        return self._database[entity_name]._vertices[name]
+    def get(self, sender, /, entity_name:str, name:str): 
+        return self._database[entity_name][name].data
 
     def flush(self, sender, /):
         self._database = {}
