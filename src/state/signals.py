632c41fed599fc13aa048ea26ddf306f08ce6d1a
Tang U-Liang <uliang6482@gmail.com>
2022-06-15 06:58:16 +0800

    Documented signals

--- src/state/signals.py
+++ src/state/signals.py
@@ -2,10 +2,11 @@ import blinker
 
 ns = blinker.Namespace()
 
-ENTRY = ns.signal('ENTRY', doc="Emitted when state is entered")
-INITIALLY_TRANSITION = ns.signal('INITIALLY_TRANSITION', doc=
-    "Emitted when parent state is entered to indicate that"
-    " transition to initial substate should occur."
+ENTRY = ns.signal("ENTRY", doc="Emitted when state is entered")
+INITIALLY_TRANSITION = ns.signal(
+    "INITIALLY_TRANSITION",
+    doc="Emitted when parent state is entered to indicate that"
+    " transition to initial substate should occur.",
 )
 
 
