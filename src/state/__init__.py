946c6c2b68de468a0280407967f351ebb9db369d
Tang U-Liang <uliang6482@gmail.com>
2022-06-21 07:17:10 +0800

    Implemented handling for exit actions
    Dispatch method was refactored so that state paths from current state
    to the destination state via the least common ancestor is calculated.
    Exit actions is executed from current state to lca while entry actions
    are executed from lca+1 to destination state followed by further
    entry actions from the destination state to its final initial states.

--- src/state/__init__.py
+++ src/state/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import ClassVar
 
-from state.signals import ENTRY, INITIALLY_TRANSITION, HANDLED
+from state.signals import ENTRY, INITIALLY_TRANSITION, HANDLED, EXIT
 from state.signals import ns
 from state.model import State
 from state.repository import StateRepository
@@ -32,6 +32,7 @@ class Entity:
             match getattr(config, name):
                 case State(
                     on_entry=handle_entry,
+                    on_exit=handle_exit, 
                     initial=initial,
                     substate_of=parent,
                     on=transition_object,
@@ -43,6 +44,10 @@ class Entity:
 
                     handler = getattr(self, handle_entry, NOOP)
                     ENTRY.connect(handler, this_state)
+
+                    handler = getattr(self, handle_exit, NOOP)
+                    EXIT.connect(handler, this_state)
+
 
                     for trigger, next_state_name in transition_object.items():
                         dest = self._repo.get(self.name, next_state_name)
@@ -71,13 +76,38 @@ class Entity:
     def dispatch(self, trigger: str, payload=None):
         signal = ns.signal(trigger)
         vp = self._current_state.clone()
+        exit_path = [] 
+        entry_path = [] 
         while not vp.points_to("ROOT"):
             for state_id in vp:
                 state = self._repo.get(self.name, name=state_id)
+                exit_path.append(state)
                 result = signal.send(state)
                 if self._current_state.changed:
                     _, dest_state_id = next(iter(result))
+                    lca = self._repo[self.name].get_lca(state_id, dest_state_id)
+                    # breakpoint()
+                    path_head = state 
+                    exit_path.append(path_head) 
+                    while path_head.name != lca.name: 
+                        path_head = self._repo.get(self.name, name=path_head.parent)
+                        exit_path.append(path_head) 
+                        # if path_head.name != lca.parent:
+                        #     break
+
                     dest_state = self._repo.get(self.name, dest_state_id)
+                    path_head = dest_state 
+                    entry_path.append(path_head)
+                    while path_head.name != lca.name: 
+                        path_head = self._repo.get(self.name, name=path_head.parent) 
+                        entry_path.append(path_head) 
+
+                    for state in exit_path: 
+                        EXIT.send(state) 
+
+                    for state in reversed(entry_path[:-1]): 
+                        ENTRY.send(state) 
+
                     INITIALLY_TRANSITION.send(dest_state)
                     self._current_state.commit()
                     return
