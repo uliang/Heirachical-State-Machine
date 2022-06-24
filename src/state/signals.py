63ad65a8a97a3405982fe6a22b8ee14134e6c543
Tang U-Liang <uliang6482@gmail.com>
2022-06-24 23:16:12 +0800

    Removed unused signals and renamed some

--- src/state/signals.py
+++ src/state/signals.py
@@ -2,15 +2,23 @@ import blinker
 
 ns = blinker.Namespace()
 
-REQUEST_LCA = ns.signal("Emitter to request lca of two nodes in a tree")
+REQUEST_VERTEX = ns.signal(
+    "REQUEST_VERTEX", doc="Emitted to recover a vertex from tree"
+)
+REQUEST_LCA = ns.signal(
+    "REQUEST_LCA", doc="Emitted to request lca of two nodes in a tree"
+)
+EXECUTE_ALONG_PATH = ns.signal(
+    "EXECUTE_ALONG_PATH",
+    doc="Emitted to request receiver to emit signals for all senders from source vertex to destination vertex",
+)
 ENTRY = ns.signal("ENTRY", doc="Emitted when state is entered")
 EXIT = ns.signal("EXIT", doc="Emitter when state is exitted")
-INITIALLY_TRANSITION = ns.signal(
-    "INITIALLY_TRANSITION",
+EXECUTE_ALONG_INITIAL_PATH = ns.signal(
+    "EXECUTE_ALONG_INITIAL_PATH",
     doc="Emitted when parent state is entered to indicate that"
     " transition to initial substate should occur.",
 )
-HANDLED = ns.signal("HANDLED", doc="Emitter when transition was handled")
 
 
 def disconnect_signals_from(namespace: blinker.Namespace):
