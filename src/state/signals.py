f105ba7e934030db88fa37270e8199386b9ea823
Tang U-Liang <uliang6482@gmail.com>
2022-06-25 16:52:45 +0800

    Removed unused signals from signals module

--- src/state/signals.py
+++ src/state/signals.py
@@ -2,16 +2,6 @@ import blinker
 
 ns = blinker.Namespace()
 
-REQUEST_VERTEX = ns.signal(
-    "REQUEST_VERTEX", doc="Emitted to recover a vertex from tree"
-)
-REQUEST_LCA = ns.signal(
-    "REQUEST_LCA", doc="Emitted to request lca of two nodes in a tree"
-)
-EXECUTE_ALONG_PATH = ns.signal(
-    "EXECUTE_ALONG_PATH",
-    doc="Emitted to request receiver to emit signals for all senders from source vertex to destination vertex",
-)
 ENTRY = ns.signal("ENTRY", doc="Emitted when state is entered")
 EXIT = ns.signal("EXIT", doc="Emitter when state is exitted")
 EXECUTE_ALONG_INITIAL_PATH = ns.signal(
