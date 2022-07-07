8534e395b5b89543af19622eb4a1747a404c91dd
Tang U-Liang <uliang6482@gmail.com>
2022-07-07 20:53:14 +0800

    Modified transitions object
    Added the logic to handle actions upon transitions. The action is
    lazily evaluated.

--- src/state/transitions.py
+++ src/state/transitions.py
@@ -1,7 +1,8 @@
 import dataclasses
 import itertools as it
+from typing import Callable
 from state.tree import Vertex
-from state.signals import INIT, ENTRY, EXIT
+from state.signals import INIT, ENTRY, EXIT 
 from state.signals import ns
 from state.signals import first, gen_result
 
@@ -11,15 +12,18 @@ class Transition:
     trigger: dataclasses.InitVar[str]
     source: dataclasses.InitVar[Vertex]
     dest: dataclasses.InitVar[Vertex]
+    action: dataclasses.InitVar[Callable] = None
 
+    _actions:  list[Callable] = dataclasses.field(init=False, default_factory=list )
     _source2dest: dict[str, Vertex] = dataclasses.field(
         init=False, default_factory=dict
     )
 
-    def __post_init__(self, trigger, source, dest):
+    def __post_init__(self, trigger, source, dest, action):
         signal = ns.signal(trigger)
         signal.connect(self, source)
-
+        if action: 
+            self._actions.append(action)
         self._source2dest[source.name] = dest
 
     def __call__(self, sender: Vertex, **payload):
@@ -36,6 +40,8 @@ class Transition:
         entry_path = it.dropwhile(lambda v: v == lca, entry_path)
 
         [EXIT.send(v) for v in exit_path]
+        for action in self._actions: 
+            action(**payload)
         [ENTRY.send(v) for v in entry_path]
 
         successors = [gen_result(INIT, dest)]
